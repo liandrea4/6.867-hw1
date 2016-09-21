da1ae1b5de8ab2d220d2dabbecc3817023b7858b
Tyson Chen <ytchen33@mit.edu>
2016-09-21 19:21:59 -0400

    Added quadratic bowl

--- code/P1/gradient_descent.py
+++ code/P1/gradient_descent.py
@@ -1,7 +1,11 @@
+import pylab
+import numpy
+
+
 def gradient_descent(objective_f, gradient_f, x0, step_size, threshold):
     old_x = x0
     old_y = objective_f(x0)
-    difference = threshold + 1:
+    difference = threshold + 1
 
     while difference > threshold:
         new_x = old_x - step_size * gradient_f(old_x)
@@ -14,3 +18,15 @@ def gradient_descent(objective_f, gradient_f, x0, step_size, threshold):
 
 def negative_gaussian(x):
     pass
+    return
+
+def make_quadratic_bowl(A, b):
+    def quadratic_bowl(x):
+        return (1/2)*numpy.matrix.transpose(x)*A*x - numpy.matrix.transpose(x)*b
+    return quadratic_bowl
+
+def make_quadratic_bowl_derivative(A, b):
+    def quadratic_bowl_derivative(x):
+        return A*x - b
+    return quadratic_bowl_derivative
+   
