f176e295cd3e53b899af120cd29bb74cba580fdb
Andrea Li <liandrea@mit.edu>
2016-09-21 20:31:05 -0400

    Add gaussian function and derivative

--- code/P1/gradient_descent.py
+++ code/P1/gradient_descent.py
@@ -1,7 +1,11 @@
+from loadParametersP1 import getData
+import numpy
+import math
+
 def gradient_descent(objective_f, gradient_f, x0, step_size, threshold):
     old_x = x0
     old_y = objective_f(x0)
-    difference = threshold + 1:
+    difference = threshold + 1
 
     while difference > threshold:
         new_x = old_x - step_size * gradient_f(old_x)
@@ -12,5 +16,24 @@ def gradient_descent(objective_f, gradient_f, x0, step_size, threshold):
 
     return (old_x, old_y)
 
-def negative_gaussian(x):
-    pass
+def make_negative_gaussian(mean, covariance):
+    def negative_gaussian(x):
+        return -1/math.sqrt((2*math.pi)**n * abs(covariance)) * math.exp(-1/2. * numpy.matrix.transpose(x-mean) / covariance * (x-mean))
+    return negative_gaussian
+
+def make_negative_gaussian_derivative(negative_gaussian, mean, covariance):
+    def negative_gaussian_derivative(x):
+        return -negative_gaussian(x) / covariance * (x-mean)
+    return negative_gaussian_derivative
+
+if __name__ == '__main__':
+    parameters = getData()
+    print "mean: ", parameters[0][0]
+    print "cov: ", abs(parameters[1])
+
+    gaussian_mean = parameters[0]
+    gaussian_cov = parameters[1]
+    negative_gaussian = make_negative_gaussian(gaussian_mean, parameters[1])
+    negative_gaussian_derivative = make_negative_gaussian_derivative(par)
+    min_x, min_y = gradient_descent(negative_gaussian)
+
