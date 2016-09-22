69aa77e08b6cc3aba3932211ea0246588d41981b
Tyson Chen <ytchen33@mit.edu>
2016-09-22 13:53:44 -0400

    Added calculate gradient numerically function

--- code/P1/gradient_descent.py
+++ code/P1/gradient_descent.py
@@ -44,20 +44,32 @@ def make_quadratic_bowl_derivative(A, b):
         return numpy.dot(A, x) - b
     return quadratic_bowl_derivative
 
+def calculate_gradient_numerically(f, x, y, delta):
+    original = numpy.array([x,y])
+    x_new = numpy.array([(x+delta), y])
+    y_new = numpy.array([x, (y+delta)])
+    x_slope = (f(x_new) - f(original))/delta
+    y_slope = (f(y_new) - f(original))/delta
+    return (x_slope, y_slope)
+
+
 if __name__ == '__main__':
     parameters = getData()
-    initial_guess = numpy.array([0, 0])
+    initial_guess = numpy.array([6, 8])
     step_size = 0.1
     threshold = 0.01
 
-    gaussian_mean = parameters[0]
-    gaussian_cov = parameters[1]
-    negative_gaussian = make_negative_gaussian(gaussian_mean, gaussian_cov)
-    negative_gaussian_derivative = make_negative_gaussian_derivative(negative_gaussian, gaussian_mean, gaussian_cov)
+    # gaussian_mean = parameters[0]
+    # gaussian_cov = parameters[1]
+    # negative_gaussian = make_negative_gaussian(gaussian_mean, gaussian_cov)
+    # negative_gaussian_derivative = make_negative_gaussian_derivative(negative_gaussian, gaussian_mean, gaussian_cov)
 
     quadratic_bowl = make_quadratic_bowl(parameters[2], parameters[3])
     quadratic_bowl_derivative = make_quadratic_bowl_derivative(parameters[2], parameters[3])
+    print quadratic_bowl_derivative(initial_guess)
+    print calculate_gradient_numerically(quadratic_bowl, 6, 8, 0.2) 
 
-    min_x, min_y = gradient_descent(negative_gaussian, negative_gaussian_derivative, initial_guess, step_size, threshold)
-    print "min_x: ", min_x, "  min_y",  min_y
+
+    # min_x, min_y = gradient_descent(negative_gaussian, negative_gaussian_derivative, initial_guess, step_size, threshold)
+    # print "min_x: ", min_x, "  min_y",  min_y
 
