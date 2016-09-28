ef685b7163a1a0d3bed8c96de4e6e8ed502f709f
Tyson Chen <ytchen33@mit.edu>
2016-09-27 23:51:21 -0400

    worked on gradient descent and sgd

--- code/P2/linear_regression.py
+++ code/P2/linear_regression.py
@@ -1,6 +1,6 @@
 import sys
 sys.path.append('../P1')
-from gradient_descent       import gradient_descent
+from gradient_descent       import gradient_descent,  make_numeric_gradient_calculator 
 from sgd                    import plot_data
 from loadFittingDataP2      import getData
 import matplotlib.pyplot    as plt
@@ -103,7 +103,7 @@ def make_sse_gradient_fn(x, y, list_of_basis_functions, M):
     return sse_gradient_fn
 
 def get_generic_regression_fn(list_of_basis_functions, w_mle):
-    if len(list_of_basis_functions) != len(w_mle):
+    if len(list_of_basis_functions) < len(w_mle):
         print "list: ", list_of_basis_functions, "   weight_vector: ", w_mle
         print "len(list): ", len(list_of_basis_functions), "   len(weight_vector): ", len(w_mle)
         raise Exception("Wrong dimensions of basis functions and weight vector")
@@ -115,13 +115,68 @@ def get_generic_regression_fn(list_of_basis_functions, w_mle):
         return fn
     return generic_regression_fn
 
+####Stochastic Gradient Descent###
+def make_single_point_least_square_error(list_of_basis_functions):
+    def single_point_least_square_error(x, y, theta):
+        regression_fn = get_generic_regression_fn(list_of_basis_functions, theta)
+        sse = y - regression_fn(x)
+        sse = sse**2
+        return sse
+    return single_point_least_square_error
+
+def calc_next_theta(old_theta, x, y, t, gradient):
+    t0 = 1000
+    k = 0.6
+    n = lambda t: (t0 + t)**(-k)
+    print "n: ", n(t), "   gradient: ", gradient(old_theta)
+    return old_theta - (n(t) * gradient(old_theta))
+
+def sgd(x, y, theta, objective_f, threshold, gradient):
+    number_of_samples = len(x)
+    old_jthetas = [0] * number_of_samples
+    differences = [False]*number_of_samples
+    previous_values = []
+    t = 0
+
+    while not all(differences):
+        i = t%number_of_samples
+        print "old_x: ", theta
+        theta = calc_next_theta(theta, x[i], y[i], t, gradient)
+        print "new_x: ", theta
+        new_jtheta = objective_f(x[i], y[i], theta)
+        difference = new_jtheta - old_jthetas[i]
+
+        if(abs(difference)<threshold):
+            differences[i] = True
+
+        previous_values.append((theta, new_jtheta))
+        print "old_jtheta: ", old_jthetas[i], "   new_jtheta: ", new_jtheta
+        print "difference: ", difference
+        old_jthetas[i] = new_jtheta
+
+        t += 1
+
+    return previous_values
 if __name__ == '__main__':
+
+#new_x:  [ 1.67582053  1.78700602  1.84132746]
+
+#new_x:  [ 0.01558979  0.54846317  0.91947502]
+
     if len(sys.argv) > 1:
         M = int(sys.argv[1])
     else:
         M = 2
     x, y = getData(ifPlotData=False)
     real_fn = lambda x: math.cos(math.pi * x) + 1.5 * math.cos(2 * math.pi * x)
+
+    basis0 = lambda x: x**0
+    basis1 = lambda x: x**1
+    basis2 = lambda x: x**2
+    basis3 = lambda x: x**3
+    basis4 = lambda x: x**4
+    basis5 = lambda x: x**5
+    list_of_basis_functions = [basis0, basis1, basis2, basis3, basis4, basis5]
 
     ## Maximum likelihood calculations
 
@@ -130,24 +185,39 @@ if __name__ == '__main__':
     # plot_regression(x, y, real_fn, regression_fn, "Linear")
 
 
-    w_mle = calculate_mle_weight(x, y, calculate_cosine_phi, M)
-    regression_fn = get_cosine_regression_fn(w_mle)
-    plot_regression(x, y, real_fn, regression_fn, "Cosine")
+    w_mle = calculate_mle_weight(x, y, calculate_polynomial_phi, M)
+    # regression_fn = get_cosine_regression_fn(w_mle)
+    # plot_regression(x, y, real_fn, regression_fn, "Cosine")
 
     print "w_mle: ", w_mle
 
 
     ## Gradient descent
-    # weight_vector = numpy.array([1] * (M+1))
-    # step_size = 0.0005
-    # threshold = 1
+    weight_vector = numpy.array([2.0] * (M+1))
+    step_size = 0.05
+    threshold = 0.0001
+    
+    single_point_objective_f = make_single_point_least_square_error(list_of_basis_functions)
 
-    # objective_f = make_sse_objective_fn(x, y, list_of_basis_functions)
-    # gradient_f = make_sse_gradient_fn(x, y, list_of_basis_functions, M)
+    # print "first value", single_point_objective_f(x[0], y[0], [ 1.67582053, 1.78700602, 1.84132746])
+    # print "second value", single_point_objective_f(x[0], y[0], [ 0.01558979, 0.54846317,  0.91947502])
 
-    # previous_values = gradient_descent(objective_f, gradient_f, weight_vector, step_size, threshold)
-    # min_x, min_y = (previous_values[-1][0], previous_values[-1][1])
-    # print "min_x: ", min_x, "  min_y",  min_y
-    # print "number of steps: ", len(previous_values)
+   
+    objective_f = make_sse_objective_fn(x, y, list_of_basis_functions)
+    gradient_f = make_sse_gradient_fn(x, y, list_of_basis_functions, M)
+    numeric_gradient = make_numeric_gradient_calculator(objective_f, 0.001)
 
-    # plot_data(previous_values, 0)
+    print "length", len(x)
+
+    previous_values = sgd(x, y, weight_vector, single_point_objective_f, threshold, numeric_gradient)
+    min_x, min_y = (previous_values[-1][0], previous_values[-1][1])
+
+    # # print "gradient", gradient_f(w_mle)
+    # print "numeric gradient", numeric_gradient(weight_vector)
+
+    # # previous_values = gradient_descent(objective_f, numeric_gradient, weight_vector, step_size, threshold)
+    # # min_x, min_y = (previous_values[-1][0], previous_values[-1][1])
+    print "min_x: ", min_x, "  min_y",  min_y
+    print "number of steps: ", len(previous_values)
+    print "w_mle: ", w_mle
+    plot_data(previous_values, 0)
